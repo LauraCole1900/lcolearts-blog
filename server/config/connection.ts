6107b202494979953dd7c5068ecd049184d8ff4c
Laura Cole <lauracole1900@comcast.net>
2021-11-28 14:09:01 -0700

    changed blog db name

--- server/config/connection.ts
+++ server/config/connection.ts
@@ -1,5 +1,5 @@
 const mongoose: any = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gcpm_db', {});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/lcoleartsblog_db', {});
 
 module.exports = mongoose.connection;
