61de3aaf1ffb7730502b7961e0926af8e89be643
Laura Cole <lauracole1900@comcast.net>
2022-04-15 21:55:19 -0600

    set up caching in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { ApolloClient, ApolloLink, ApolloProvider, createHttpLink, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
+import { setContext } from "@apollo/client/link/context";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from "react-bootstrap/Container";
 import Navigation from "./components/navbar";
@@ -8,9 +9,44 @@ import { Blog, BlogEntry, LoginPage, NotFound } from "./components/pages";
 import Footer from "./components/footer";
 import './App.css';
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
+
+//=====================//
+//      Functions      //
+//=====================//
+
+// Creates Apollo Client http link for GraphQL operations
+const httpLink: ApolloLink = createHttpLink({
+  uri: "/graphql"
+});
+
+// Sets authentication into context
+const authLink: ApolloLink = setContext((_, { headers }) => {
+  const token: string | null = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+});
+
+// Instantiates the client object and the cache object with some specific options
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          oneConcert: {
+            merge: true
+          },
+          addRepertoire: {
+            merge: true
+          }
+        }
+      }
+    }
+  })
 });
 
 const App = (): ReactElement => {
