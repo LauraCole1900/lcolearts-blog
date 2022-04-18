63b11b0c975e2b858c36f9068b2e2bf53db07e65
Laura Cole <lauracole1900@comcast.net>
2022-04-18 08:25:08 -0600

    added confirmModal.tsx

---
+++ client/src/components/modals/confirmModal.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from "react";
+import { Button, Modal } from "react-bootstrap";
+import "./style.css";
+
+
+const ConfirmModal = (props: any): ReactElement => {
+
+
+  return (
+    <>
+      <Modal show={props.show} onHide={props.hide} backdrop="static" keyboard={false} centered={true} className="modal">
+        <Modal.Header className="modalHeadConf">
+          <Modal.Title className="modalTitle"><h2>Please confirm</h2></Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="modalBody">
+
+          {/* Delete Post button */}
+          <p>Are you sure you want to delete {props.postTitle}? This action can't be undone.</p>
+
+          {/* Navigation buttons */}
+          <Modal.Footer className="modalFooter">
+
+            {/* Confirm Delete Post button */}
+            <Button data-toggle="popover" title="Confirm Delete" className="button" type="button" onClick={props.postDelete}>Yes, Delete Post</Button>
+
+            {/* No, take no action button */}
+            <Button data-toggle="popover" title="No" className="button" type="button" onClick={props.hide}>No, take me back</Button>
+
+          </Modal.Footer>
+
+        </Modal.Body>
+      </Modal>
+    </>
+  )
+}
+
+export default ConfirmModal;
--- client/src/components/modals/index.js
+++ client/src/components/modals/index.js
@@ -1,2 +1,3 @@
+export { default as ConfirmModal } from "./confirmModal";
 export { default as ErrorModal } from "./errorModal";
 export { default as SuccessModal } from "./successModal";
