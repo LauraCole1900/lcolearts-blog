3b303a56bc806a8a153e8dc2f99bd2ebc362f9f5
Laura Cole <lauracole1900@comcast.net>
2024-01-18 14:00:09 -0700

    song sort on multiple keys works ascending, but not descending

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -1,33 +1,52 @@
 import { Song } from "./interfaces";
 
 const removeArticles = (str: string): string | void => {
-  const words: string[] = str.split(' ');
-  if (words.length <= 1) return str
-  if (['A', 'An', 'The'].includes(words[0])) {
-    return words.splice(1).join(' ') + ', ' + words[0];
+  const words: string[] = str.split(" ");
+  if (words.length <= 1) return str;
+  if (["A", "An", "The"].includes(words[0])) {
+    return words.splice(1).join(" ") + ", " + words[0];
   }
   return str;
-}
+};
+
+const compare = (
+  a: string | boolean | Array<string> | undefined,
+  b: string | boolean | Array<string> | undefined,
+  aTitle: string,
+  bTitle: string
+): 1 | -1 => {
+  if (a === b) {
+    return aTitle > bTitle ? 1 : -1;
+  } else {
+    return a! > b! ? 1 : -1;
+  }
+};
 
 export const sortAsc = (data: Array<Song>, col: string): Array<Song> => {
   const arrToSort: Array<Song> = [...data];
   const sortedData: Array<Song> = arrToSort.sort((a: Song, b: Song): 1 | -1 => {
+    let aTitle: string | void = removeArticles(a.songTitle);
+    let bTitle: string | void = removeArticles(b.songTitle);
     if (col === "songTitle") {
-      let aTitle: string | void = removeArticles(a.songTitle);
-      let bTitle: string | void = removeArticles(b.songTitle);
-      return aTitle > bTitle ? 1 : -1
+      return aTitle > bTitle ? 1 : -1;
     } else if (col === "songAccompaniment") {
       let aAcc: string = a.songAccompaniment.toLowerCase();
       let bAcc: string = b.songAccompaniment.toLowerCase();
-      return aAcc > bAcc ? 1 : -1
+      return compare(aAcc, bAcc, aTitle!, bTitle!);
     } else if (col === "songSacred") {
-      return a.songSacred > b.songSacred ? -1 : 1;
+      return compare(b.songSacred, a.songSacred, aTitle!, bTitle!);
     } else {
-      return a[col]! > b[col]! ? 1 : -1
+      return compare(a[col], b[col], aTitle!, bTitle!);
     }
   });
   return sortedData;
 };
+
+// const sortedPres = moreFilteredPres.sort(
+//   firstBy("presKeynote", "desc")
+//     .thenBy("presFamilyName")
+//     .thenBy("presGivenName")
+// );
 
 export const sortDesc = (data: Array<Song>, col: string): Array<Song> => {
   const arrToSort: Array<Song> = [...data];
@@ -35,15 +54,15 @@ export const sortDesc = (data: Array<Song>, col: string): Array<Song> => {
     if (col === "songTitle") {
       let aTitle: string | void = removeArticles(a.songTitle);
       let bTitle: string | void = removeArticles(b.songTitle);
-      return aTitle > bTitle ? -1 : 1
+      return aTitle > bTitle ? -1 : 1;
     } else if (col === "songAccompaniment") {
       let aAcc: string = a.songAccompaniment.toLowerCase();
       let bAcc: string = b.songAccompaniment.toLowerCase();
-      return aAcc > bAcc ? -1 : 1
+      return aAcc > bAcc ? -1 : 1;
     } else if (col === "songSacred") {
       return a.songSacred > b.songSacred ? 1 : -1;
     } else {
-      return a[col]! > b[col]! ? -1 : 1
+      return a[col]! > b[col]! ? -1 : 1;
     }
   });
   return sortedData;
