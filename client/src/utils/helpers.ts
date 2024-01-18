a1f86ed4225f6cea1a1ca832957e79527fbd9edd
Laura Cole <lauracole1900@comcast.net>
2024-01-18 14:50:19 -0700

    song sort on multiple keys works descending

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -13,12 +13,22 @@ const compare = (
   a: string | boolean | Array<string> | undefined,
   b: string | boolean | Array<string> | undefined,
   aTitle: string,
-  bTitle: string
+  bTitle: string,
+  dir: string
 ): 1 | -1 => {
-  if (a === b) {
-    return aTitle > bTitle ? 1 : -1;
-  } else {
-    return a! > b! ? 1 : -1;
+  console.log(a, b);
+  if (dir === "asc") {
+    if (a === b) {
+      return aTitle > bTitle ? 1 : -1;
+    } else {
+      return a! > b! ? 1 : -1;
+    }
+  } else if (dir === "dsc") {
+    if (a === b) {
+      return aTitle > bTitle ? 1 : -1;
+    } else {
+      return a! > b! ? -1 : 1;
+    }
   }
 };
 
@@ -27,42 +37,38 @@ export const sortAsc = (data: Array<Song>, col: string): Array<Song> => {
   const sortedData: Array<Song> = arrToSort.sort((a: Song, b: Song): 1 | -1 => {
     let aTitle: string | void = removeArticles(a.songTitle);
     let bTitle: string | void = removeArticles(b.songTitle);
+    let aVoicing: string | void = a.songVoicing;
+    let bVoicing: string | void = b.songVoicing;
     if (col === "songTitle") {
       return aTitle > bTitle ? 1 : -1;
     } else if (col === "songAccompaniment") {
       let aAcc: string = a.songAccompaniment.toLowerCase();
       let bAcc: string = b.songAccompaniment.toLowerCase();
-      return compare(aAcc, bAcc, aTitle!, bTitle!);
+      return compare(aAcc, bAcc, aTitle!, bTitle!, "asc");
     } else if (col === "songSacred") {
-      return compare(b.songSacred, a.songSacred, aTitle!, bTitle!);
+      return compare(b.songSacred, a.songSacred, aTitle!, bTitle!, "asc");
     } else {
-      return compare(a[col], b[col], aTitle!, bTitle!);
+      return compare(a[col], b[col], aTitle!, bTitle!, "asc");
     }
   });
   return sortedData;
 };
 
-// const sortedPres = moreFilteredPres.sort(
-//   firstBy("presKeynote", "desc")
-//     .thenBy("presFamilyName")
-//     .thenBy("presGivenName")
-// );
-
 export const sortDesc = (data: Array<Song>, col: string): Array<Song> => {
   const arrToSort: Array<Song> = [...data];
   const sortedData: Array<Song> = arrToSort.sort((a: Song, b: Song): -1 | 1 => {
+    let aTitle: string | void = removeArticles(a.songTitle);
+    let bTitle: string | void = removeArticles(b.songTitle);
     if (col === "songTitle") {
-      let aTitle: string | void = removeArticles(a.songTitle);
-      let bTitle: string | void = removeArticles(b.songTitle);
-      return aTitle > bTitle ? -1 : 1;
+      return aTitle > bTitle ? 1 : -1;
     } else if (col === "songAccompaniment") {
       let aAcc: string = a.songAccompaniment.toLowerCase();
       let bAcc: string = b.songAccompaniment.toLowerCase();
-      return aAcc > bAcc ? -1 : 1;
+      return compare(aAcc, bAcc, aTitle!, bTitle!, "dsc");
     } else if (col === "songSacred") {
-      return a.songSacred > b.songSacred ? 1 : -1;
+      return compare(b.songSacred, a.songSacred, aTitle!, bTitle!, "dsc");
     } else {
-      return a[col]! > b[col]! ? -1 : 1;
+      return compare(a[col], b[col], aTitle!, bTitle!, "dsc");
     }
   });
   return sortedData;
