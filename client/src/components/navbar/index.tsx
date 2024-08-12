afc89a1a76c4226ee63f99fdf81bd08c37ecdb1f
Laura Cole <lauracole1900@comcast.net>
2024-08-12 11:03:23 -0600

    fixed navbar links

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -11,33 +11,33 @@ const Navigation = (): ReactElement => {
   return (
     <Navbar expand="sm" className="navbar">
       <Navbar.Brand>
-        <a href="https://lcolearts.com/#/about" className="mylogo">
+        <a href="https://lcolearts.com/about" className="mylogo">
           Laura Cole
         </a>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <a href="https://lcolearts.com/#/" className="navlink">
+          <a href="https://lcolearts.com/" className="navlink">
             Home
           </a>
-          <a href="https://lcolearts.com/#/about" className="navlink">
+          <a href="https://lcolearts.com/about" className="navlink">
             About
           </a>
-          <a href="https://lcolearts.com/#/webdev" className="navlink">
+          <a href="https://lcolearts.com/webdev" className="navlink">
             Webdev
           </a>
           <NavDropdown title="Musician">
-            <NavDropdown.Item href="https://lcolearts.com/#/musician" className="navlink">C.V.</NavDropdown.Item>
+            <NavDropdown.Item href="https://lcolearts.com/musician" className="navlink">C.V.</NavDropdown.Item>
             <NavDropdown.Item href="/music/page/1" className="navlink" rel="noreferred noopener">Compositions</NavDropdown.Item>
           </NavDropdown>
-          <a href="https://lcolearts.com/#/martial_artist" className="navlink">
+          <a href="https://lcolearts.com/martial_artist" className="navlink">
             Martial Artist
           </a>
           <Link to="/blog/page/1" className="navlink">
             Blog
           </Link>
-          <a href="https://lcolearts.com/#/fun_and_games" className="navlink">
+          <a href="https://lcolearts.com/fun_and_games" className="navlink">
             Fun & Games
           </a>
           {/* <a href="https://lcolearts.com/#/contact" className="navlink">
